dcade0bc44a4ecbaf929b203303560e890c18680
Aaraamis <130172438+Aaraamis@users.noreply.github.com>
2024-12-23 10:07:34 -0500

    Aaraamis.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Aaraamis.py
@@ -0,0 +1,10 @@
+# https://www.python.org/
+# comentario en una linea
+
+"""
+Esto tambien es un comentario
+en varias lines 
+"""
+
+variable = "Mi variable"
+CONSTANTE = 59 # Por convencion no existen constantes en python
